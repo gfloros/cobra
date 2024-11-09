73079491847a62cdadd8b76f9d51cc718b7fc761
Georgios Floros <georgios.floros@dronedeploy.com>
2024-11-09 11:50:22 +0200

    cleanup preprocessing script

--- preprocess.py
+++ preprocess.py
@@ -1,130 +1,141 @@
+""" Preprocess 3D models by sampling points from the models. """
+
+import dataclasses
+import os
+from pathlib import Path
+import shutil
+from typing import List
+import tyro
+
+from rich.console import Console
+import open3d as o3d
+
+import common
+
 from utils.model_3d import (
     sample_points_from_mesh,
-    sample_surface_points_trimesh,
     sample_visible_surface,
     load_mesh,
     fitModel2UnitSphere,
     get_model_size,
 )
 from utils.io import make_dir, log_event
-import tyro
-import dataclasses
-from typing import List
-import os
-import common
-from os.path import join as jn
-from rich.console import Console
-import open3d as o3d
-import shutil
 
 CONSOLE = Console()
+NUM_SAMPLES = [10000, 250000]
+TRAIN_RANDOM_SEED = 42
+TEST_RANDOM_SEED = 102
 
 
 @dataclasses.dataclass
-class sample_points_args:
-
-    # class name
+class SamplePointsArgs:
+    """ Arguments for sampling points from 3D models """
     class_name: str
-
-    # path to original meshes folder
-    data_path: str = common.MODELS_PATH + "/original"
-
-    # normalize the point cloud
     normalize: bool = True
-
-    # Number of sampling points for train and test splits
-    num_samples: List[int] = dataclasses.field(default_factory=lambda: [10000, 250000])
-
+    num_samples: List[int] = dataclasses.field(
+        default_factory=lambda: NUM_SAMPLES
+    )
     visible: bool = True
 
 
-def run(args):
+class Preprocess:
+    """ Preprocess 3D models by sampling points from the models. """
+    def __init__(self, cmd_args):
+        self.sample_points_args = cmd_args
+        self.original_dir = Path(common.MODELS_PATH) / "original"
+        self.normalized_dir = Path(common.MODELS_PATH) / "normalized"
+        self.training_dir = Path(common.MODELS_PATH) / "train"
+        self.test_dir = Path(common.MODELS_PATH) / "test"
+        name = self.sample_points_args.class_name
+        self.model_original_dir = self.original_dir / name
+        self.model_training_dir = self.training_dir / name
+        self.model_test_dir = self.test_dir / name
+        self.model_normalized_dir = self.normalized_dir / name
+        self.models = os.listdir(self.original_dir / name)
+        make_dir(self.training_dir)
+        make_dir(self.test_dir)
+        make_dir(self.normalized_dir)
+        make_dir(self.model_training_dir)
+        make_dir(self.model_test_dir)
+        make_dir(self.model_normalized_dir)
 
-    make_dir(common.MODELS_PATH + "/train")
-    make_dir(common.MODELS_PATH + "/test")
+    def normalize_model(self, model: str):
+        """ Normalize the model by fitting it to a unit sphere.
 
-    make_dir(common.MODELS_PATH + "/train/" + f"/{args.class_name}")
-    make_dir(common.MODELS_PATH + "/test/" + f"/{args.class_name}")
-    make_dir(common.MODELS_PATH + "/normalized" + f"/{args.class_name}")
+        Args:
+            model_path (str): Path to the model.
+            output_path (str): Path to save the normalized model.
+        """
+        model_filepath = self.model_original_dir / model
+        vertices, triangles = load_mesh(model_filepath.absolute().as_posix())
+        normalized_vetcies = fitModel2UnitSphere(vertices)
+        mesh = o3d.geometry.TriangleMesh()
+        mesh.vertices = o3d.utility.Vector3dVector(normalized_vetcies)
+        mesh.triangles = o3d.utility.Vector3iVector(triangles)
+        mesh.compute_vertex_normals()
+        output_path = self.model_normalized_dir / model
+        o3d.io.write_triangle_mesh(output_path.absolute().as_posix(), mesh)
 
-    for model in os.listdir(jn(args.data_path, args.class_name)):
+    def sample_visible_surface(self, model: str):
+        """ Sample points from the visible surface of the model.
 
-        if args.normalize:
-            # save the normalized model
-            original_model = load_mesh(
-                jn(common.MODELS_PATH, "original", args.class_name, model)
-            )
-            normalized_vetcies = fitModel2UnitSphere(original_model[0])
-            mesh = o3d.geometry.TriangleMesh()
-            mesh.vertices = o3d.utility.Vector3dVector(normalized_vetcies)
-            mesh.triangles = o3d.utility.Vector3iVector(original_model[1])
+        Args:
+            model (str): Model name.
+        """
+        model_filepath = self.model_normalized_dir / model
+        sample_visible_surface(
+            num_sample_centers=100,
+            mesh_path=model_filepath.absolute().as_posix(),
+            num_train=self.sample_points_args.num_samples[0],
+            num_test=self.sample_points_args.num_samples[1],
+            model_class=self.sample_points_args.class_name,
+            scale_fn=get_model_size,
+            model_name=model,
+            save_sampling_positions=True,
+        )
 
-            # Optional: Compute normals for better visualization
-            mesh.compute_vertex_normals()
+    def sample_mesh(self, model: str):
+        """ Sample points from the mesh.
 
-            # Step 4: Save the mesh to a file (e.g., .ply format)
-            o3d.io.write_triangle_mesh(
-                common.MODELS_PATH
-                + "/normalized"
-                + f"/{args.class_name}/{os.path.basename(model)}",
-                mesh,
-            )
-        else:
+        Args:
+            model (str): Model name.
+        """
+        model_filepath = self.model_normalized_dir / model
+        train_output_path = self.model_training_dir / f"{Path(model).stem}.ply"
+        test_output_path = self.model_test_dir / f"{Path(model).stem}.ply"
+        sample_points_from_mesh(
+            model_filepath.absolute().as_posix(),
+            self.sample_points_args.num_samples[0],
+            random_seed=TRAIN_RANDOM_SEED,
+            output_path=train_output_path.absolute().as_posix(),
+        )
+        sample_points_from_mesh(
+            model_filepath.absolute().as_posix(),
+            self.sample_points_args.num_samples[1],
+            random_seed=TEST_RANDOM_SEED,
+            output_path=test_output_path.absolute().as_posix(),
+        )
 
-            # just coppy the original model into the normalized models path
-            shutil.copy(
-                jn(args.data_path, args.class_name, model),
-                jn(
-                    args.data_path.replace("original", "normalized"),
-                    args.class_name,
-                    model,
-                ),
-            )
-
-        log_event(f"Sampling points for model {model}")
-        # sample training points
-        if args.visible:
-            log_event("Sampling training points")
-            sample_visible_surface(
-                num_sample_centers=100,
-                mesh_path=jn(
-                    args.data_path.replace("original", "normalized"),
-                    args.class_name,
-                    model,
-                ),
-                num_train=args.num_samples[0],
-                num_test=args.num_samples[1],
-                model_class=args.class_name,
-                scale_fn=1.0 if args.normalize else get_model_size,
-                model_name=model,
-                save_sampling_positions=True,
-            )
-        else:
-            sample_points_from_mesh(
-                jn(args.data_path, args.class_name, model),
-                args.num_samples[0],
-                random_seed=42,
-                output_path=common.MODELS_PATH
-                + "/train/"
-                + f"/{args.class_name}/"
-                + model.split(".")[0]
-                + ".ply",
-            )
-            # sample test points
-            sample_points_from_mesh(
-                jn(args.data_path, args.class_name, model),
-                args.num_samples[1],
-                random_seed=102,
-                output_path=common.MODELS_PATH
-                + "/test/"
-                + f"/{args.class_name}/"
-                + model.split(".")[0]
-                + ".ply",
-            )
-
-    log_event("Done sampling points.")
+    def run(self):
+        """ Run the preprocessing pipeline. """
+        for model in self.models:
+            if self.sample_points_args.normalize:
+                log_event(f"Normalizing model {model}")
+                self.normalize_model(model)
+            else:
+                shutil.copy(
+                    self.model_original_dir / model,
+                    self.model_normalized_dir / model,
+                )
+            if self.sample_points_args.visible:
+                log_event("Sampling training points from visible surface")
+                self.sample_visible_surface(model)
+            else:
+                log_event("Sampling training points from mesh")
+                self.sample_mesh(model)
 
 
 if __name__ == "__main__":
-    args = tyro.cli(sample_points_args)
-    run(args)
+    sample_points_args = tyro.cli(SamplePointsArgs)
+    preprocess = Preprocess(sample_points_args)
+    preprocess.run()
